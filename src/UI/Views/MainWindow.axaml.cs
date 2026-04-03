eefeed05e912f97bd01d45e0b474fdbcecf179ee
Ayoze Aguiar <ayagunu@gmail.com>
2026-04-03 23:02:06 +0100

    fix(ui): cerrar->tray, minimizar->barra de tareas normal

--- src/UI/Views/MainWindow.axaml.cs
+++ src/UI/Views/MainWindow.axaml.cs
@@ -8,18 +8,6 @@ public partial class MainWindow : Window
     public MainWindow()
     {
         InitializeComponent();
-
-        PropertyChanged += (_, e) =>
-        {
-            if (e.Property == WindowStateProperty
-                && WindowState == WindowState.Minimized
-                && DataContext is MainWindowViewModel vm
-                && vm.Settings.MinimizeToTray)
-            {
-                Hide();
-                WindowState = WindowState.Normal;
-            }
-        };
     }
 
     protected override void OnClosing(WindowClosingEventArgs e)
