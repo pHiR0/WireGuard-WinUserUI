2e4b769021569bb0eda06a07ca5c42e91bc7c42b
Ayoze Aguiar <ayagunu@gmail.com>
2026-04-04 19:58:49 +0100

    fix(choco): corregir uninstall MSI usando PSChildName (GUID) en silentArgs

--- packaging/wireguard-manager/tools/chocolateyuninstall.ps1
+++ packaging/wireguard-manager/tools/chocolateyuninstall.ps1
@@ -4,20 +4,20 @@ $packageArgs = @{
   packageName   = $env:ChocolateyPackageName
   softwareName  = 'WireGuard Manager*'
   fileType      = 'msi'
-  silentArgs    = '/quiet /norestart'
   validExitCodes= @(0, 3010)
 }
 
-$uninstalled = $false
 [array]$key = Get-UninstallRegistryKey -SoftwareName $packageArgs['softwareName']
 
 if ($key.Count -eq 1) {
   $key | ForEach-Object {
-    $packageArgs['file'] = "$($_.UninstallString)"
+    # Para MSI: PSChildName es el GUID del producto ({XXXX-...})
+    # msiexec /x {GUID} /qn /norestart
+    $packageArgs['silentArgs'] = "$($_.PSChildName) /qn /norestart"
     Uninstall-ChocolateyPackage @packageArgs
   }
 } elseif ($key.Count -eq 0) {
-  Write-Warning "$packageName has already been uninstalled by other means."
+  Write-Warning "$($packageArgs['packageName']) has already been uninstalled by other means."
 } elseif ($key.Count -gt 1) {
   Write-Warning "$($key.Count) matches found!"
   Write-Warning "To prevent accidental removal, uninstall manually."
