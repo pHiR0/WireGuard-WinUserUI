4466cf64da9525bfe787098ab9c0e7cfe2e5d139
Ayoze Aguiar <ayagunu@gmail.com>
2026-04-04 01:10:05 +0100

    fix(ui): aniadir using System.Reflection para GetCustomAttribute en AppVersion

--- src/UI/ViewModels/MainWindowViewModel.cs
+++ src/UI/ViewModels/MainWindowViewModel.cs
@@ -3,6 +3,7 @@ using System.IO;
 using System.Collections.Generic;
 using System.Collections.ObjectModel;
 using System.Linq;
+using System.Reflection;
 using System.Threading;
 using System.Threading.Tasks;
 using Avalonia.Threading;
