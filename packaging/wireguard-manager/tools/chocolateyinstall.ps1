fa42aa9c9e100d52f4d18a56bde2ae04f689f4a9
Ayoze Aguiar <ayagunu@gmail.com>
2026-04-04 20:49:17 +0100

    fix(package): update version to 26.4.4.2029 and correct download URL in chocolateyinstall.ps1

--- packaging/wireguard-manager/tools/chocolateyinstall.ps1
+++ packaging/wireguard-manager/tools/chocolateyinstall.ps1
@@ -3,8 +3,8 @@
 $packageArgs = @{
   packageName   = $env:ChocolateyPackageName
   fileType      = 'msi'
-  url64bit      = 'https://github.com/pHiR0/WireGuard-WinUserUI/releases/download/v26.04.04.0310/WireGuard-WinUserUI-26.04.04.0310-x64.msi'  # URL del release; la versión del paquete Chocolatey (26.4.4.310) difiere por normalización NuGet
-  checksum64    = 'ba0ffc7ceb015b9a220e7aa756f47659ef3f4d9ec3069a2ff7fe12d63df1e41f'
+  url64bit      = 'https://github.com/pHiR0/WireGuard-WinUserUI/releases/download/v26.4.4.2029/WireGuard-WinUserUI-26.4.4.2029-x64.msi'  # URL del release; la versión del paquete Chocolatey (26.4.4.310) difiere por normalización NuGet
+  checksum64    = 'c550b260a2a4d98640421935756a110c2992bc2f6da50321633d4ad77ef0f1ee'
   checksumType64= 'sha256'
   silentArgs    = '/quiet /norestart'
   validExitCodes= @(0, 3010)
